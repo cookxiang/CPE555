66f89214398eb86f13a5cce317ec810d192cbc90
Jacob <jxiang@stevens.edu>
2015-12-07 23:38:22 -0500

    Server for 555

--- Server/servermain.cc
+++ Server/servermain.cc
@@ -4,26 +4,17 @@
 
 
 int main() {
-    
-    vector<int> connfd;
-    vector<thread> t_read;
-    vector<thread> t_write;
-    int i=0;
-    
-    
-    Server s1(1510); // read port
+    int connfd;
+    Server s1(555);
     s1.server_addr_init();
     s1.bind_listen();
-    
     while(1) {
-        connfd.push_back(s1.accept_client());
-        cout << "connfd[" << i << "] = " << connfd.back() << endl;
-        s1.recieve_client_list(connfd.back());
-        s1.send_client_list(connfd.back());
-        t_read.push_back(thread(Server::str_read, connfd.back()));
-        t_write.push_back(thread(Server::str_write, connfd.back()));
-        i++;
+        connfd = s1.accept_client();
+        char buffer[50];
+        cin >> buffer;
+        write(connfd, buffer, 50);
+        close(connfd);
+        cout << "Transfer complete!" << endl;
     }
-
     
 }
