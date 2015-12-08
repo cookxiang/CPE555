92cff0b7692411ef5cedcf935e165f37631e95fc
Jacob <jxiang@stevens.edu>
2015-12-07 23:48:04 -0500

    Server finished

--- Server/servermain.cc
+++ Server/servermain.cc
@@ -5,14 +5,16 @@
 
 int main() {
     int connfd;
-    Server s1(555);
+    Server s1(10500);
     s1.server_addr_init();
     s1.bind_listen();
     while(1) {
         connfd = s1.accept_client();
-        char buffer[50];
-        cin >> buffer;
-        write(connfd, buffer, 50);
+        cout << "Connected!" << endl;
+        char buffer1[50]="1.5";
+        char buffer2[50]="2.4";
+        write(connfd, buffer1, 50);
+        write(connfd, buffer2, 50);
         close(connfd);
         cout << "Transfer complete!" << endl;
     }
